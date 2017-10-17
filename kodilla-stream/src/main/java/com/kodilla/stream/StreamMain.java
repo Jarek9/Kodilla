93826816c31b8bd9565e3c2a43a2708c108565fd
Jarek9 <jbabecki@wp.pl>
2017-10-17 23:49:30 +0200

    update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,23 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.SaySomething;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
-        System.out.println("Welcome to module 7 - Stream");
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
 
-        SaySomething saySomething = new SaySomething();
-        saySomething.say();
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
     }
 }
+
