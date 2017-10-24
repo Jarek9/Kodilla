aac852ab4cfc6bfb2fe7f49b36e0185cccb9fc18
Jarek9 <jbabecki@wp.pl>
2017-10-24 22:20:27 +0200

    update

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,12 +1,17 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.test.SecondChallenge;
 
 public class ExceptionModuleRunner {
 
     public static void main(String args[]) {
 
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(1,1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
