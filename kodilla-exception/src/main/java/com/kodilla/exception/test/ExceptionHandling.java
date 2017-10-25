2f3f1af0115990ca06f96c07b77f724d2310b0f3
Jarek9 <jbabecki@wp.pl>
2017-10-25 10:10:05 +0200

    update

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,15 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling extends Exception{
+
+    public static void main(String args[]) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(1,1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
