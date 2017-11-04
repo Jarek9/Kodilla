405fa39b787c7964c9d33478f2c64b9ac99d3456
Jarek9 <jbabecki@wp.pl>
2017-11-04 10:39:37 +0100

    update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/E_Store/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/E_Store/Application.java
@@ -3,15 +3,12 @@ package com.kodilla.good.patterns.challenges.E_Store;
 public class Application {
 
     public static void main(String[] args) {
-
         OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
         OrderRequest orderRequest = orderRequestRetriever.retrive();
 
         ProductOrderService productOrderService = new ProductOrderService(
                 new MailService(), new CompletionOrderService(), new ProductOrderRepository());
         productOrderService.process(orderRequest);
-
-
 
     }
 }
