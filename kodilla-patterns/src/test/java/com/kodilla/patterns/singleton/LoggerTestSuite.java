8d0b087d5293c86b4b8cdb938edd7f22aa77a1de
Jarek9 <jbabecki@wp.pl>
2017-11-16 21:04:35 +0100

    Task_11.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+
+    @BeforeClass
+    public static void logged() {
+        Logger.getInstance().log("login attempt, user: jarekb");
+
+    }
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        System.out.println("Last log: " + lastLog);
+        //Then
+        Assert.assertEquals("login attempt, user: jarekb", lastLog);
+    }
+}
