395c4ae4b54e21d35f06baa6acaf272c17d881c8
Jarek9 <jbabecki@wp.pl>
2018-01-21 13:58:19 +0100

    Task_20.1.2

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/DatabaseSearcherException.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/facade/DatabaseSearcherException.java
@@ -2,10 +2,10 @@ package com.kodilla.hibernate.manytomany.facade;
 
 public class DatabaseSearcherException extends Exception {
 
-    public static String ERR_NO_EMPLOYEE_FOUND = "No employee match the searched parameters";
-    public static String ERR_NO_COMPANY_FOUND = "No company match the searched parameters";
-    public static String ERR_SEARCH_FINISHED = "Searching process was finished unsuccessful";
-    public static String INFO_SEARCH_FINISHED = "Searching process was completed";
+    public static String ERR_NO_EMPLOYEE_FOUND = "No employee found";
+    public static String ERR_NO_COMPANY_FOUND = "No company found";
+    public static String ERR_SEARCH_FINISHED = "Searching error";
+    public static String INFO_SEARCH_FINISHED = "Search completed";
 
     public DatabaseSearcherException(String message) {
         super(message);
